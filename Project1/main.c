30d4abac6f25f441c0989ab260b2923f81960ece
Ernesto Garc√≠a <Ernesto@ernestos-macbook-pro.local>
2015-09-21 23:00:40 -0500

    Initial Commit

---
+++ Project1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project1
+//
+//  Created by Ernesto García on 21/09/15.
+//  Copyright © 2015 Ernesto García. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
