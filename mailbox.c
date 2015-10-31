951322456c06e80f8ea8cea039d479b8165aaa8c
Geet Kumar <gkumar7@hawk.iit.edu>
2015-10-31 10:57:27 -0500

    linked list: create_mailbox, add_to_mailbox

--- mailbox.c
+++ mailbox.c
@@ -1,9 +1,55 @@
 /* File mailbox.c */
 /* Define system calls to the mailbox */
 
-#define MAILBOX_SIZE 16
+#include "mailboxlib.h"
 
 
+static mailbox_t *mailbox;
+
+int create_mailbox(){
+  mailbox = malloc(sizeof(mailbox_t));
+  mailbox->messages = malloc(sizeof(message_t));
+  mailbox->last_msg = malloc(sizeof(message_t));
+  mailbox->number_of_messages = 0;
+  return OK;
+}
+
+/* Creates mailbox if there is none
+ * Add message to mailbox (if mailbox is not full)
+ * Returns OK if message was successfully added
+ * Returns ERROR if mailbox is full
+ *
+ * TODO: Remove parameters and use a message type
+ */
+int add_to_mailbox(char *message, int *recipients, int recipients_num){
+  if (!mailbox){
+    create_mailbox();
+  }
+
+  message_t *new_message = malloc(sizeof(message_t));
+  new_message->recipients_num = recipients_num;
+  new_message->recipients = recipients;
+  new_message->message = message;
+  new_message->next = NULL;
+
+  if (mailbox->number_of_messages < MAX_MESSAGE_COUNT){
+    if (!(mailbox->messages->message)){
+      mailbox->messages = new_message;
+      mailbox->last_msg = new_message;
+    }
+    else {
+      mailbox->last_msg->next = new_message;
+      mailbox->last_msg = new_message;
+    }
+    mailbox->number_of_messages += 1;
+  }
+  else {
+    printf("Error: mailbox is full\n");
+    return ERROR;
+  }
+
+  return OK;
+}
 
 /* Send system call */
 int do_send()
--- mailboxlib.h
+++ mailboxlib.h
@@ -2,36 +2,38 @@
  * Define prototypes for message and mailbox structs *
  * ================================================= */
 
+#include <stdlib.h>
+#include <stdio.h>
 
-/* TODO: We need a new struct: Linked list */
+#define MAX_MESSAGE_COUNT 16
+#define OK 0
+#define ERROR -1
 
-/* Message
- * received - initially 0, incremented each time a recipient receives its message
- * recipients_num - total number of recipients
+/* Message LinkedList
+ * recipients_num - total number of recipients (will be decremented each time
+                    this message is retrieved by one of its recipients
  * recipients - recipients of this message
  * message - the message value
+ * next - pointer to next message
  */
 
-typedef struct {
-  int received;
+typedef struct message_struct {
   int recipients_num;
   int *recipients;
   char *message;
+  struct message_struct *next;
 } message_t;
 
 /* Mailbox
  * number_of_messages - current number of messages in the mailbox (limit is 16)
- * messages - array of message_t objects
- * open_slots - when a message is garbage collected, add its index here
- *              (if empty, use last_message_index for a new message)
- * last_message_index - index of the last message in the mailbox
+ * messages - pointer to head of message linked list
+ * last_msg - pointer to last message of the linked list
  */
 
 typedef struct {
   int number_of_messages;
   message_t *messages;
-  int *open_slots;
-  int last_message_index;
+  message_t *last_msg;
 } mailbox_t;
 
 
