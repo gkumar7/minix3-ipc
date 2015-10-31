3c85515a70a645c6c653f8de3c1958632742795a
Sergio Gil <sergio.gil.luque@gmail.com>
2015-10-31 12:53:48 +0000

    Cleaning

--- mailbox.c
+++ mailbox.c
@@ -3,15 +3,14 @@
 
 #include <mailboxlib.h>
 
-
 static mailbox_t *mailbox;
 
 int create_mailbox(){
-  mailbox = malloc(sizeof(mailbox_t));
-  mailbox->messages = malloc(sizeof(message_t));
-  mailbox->last_msg = malloc(sizeof(message_t));
-  mailbox->number_of_messages = 0;
-  return OK;
+	mailbox = malloc(sizeof(mailbox_t));
+	mailbox->messages = malloc(sizeof(message_t));
+	mailbox->last_msg = malloc(sizeof(message_t));
+	mailbox->number_of_messages = 0;
+	return OK;
 }
 
 /* Creates mailbox if there is none
@@ -21,32 +20,38 @@ int create_mailbox(){
  *
  * TODO: Remove parameters and use a message type
  */
-int add_to_mailbox(char *message, int *recipients, int recipients_num){
-  if (!mailbox){
-    create_mailbox();
-  }
+int add_to_mailbox(char *message, int *recipients, int recipients_num)
+{
+	if (!mailbox)
+	{
+		create_mailbox();
+	}
 
-  message_t *new_message = malloc(sizeof(message_t));
-  new_message->recipients_num = recipients_num;
-  new_message->recipients = recipients;
-  new_message->message = message;
-  new_message->next = NULL;
+	message_t *new_message = malloc(sizeof(message_t));
+	new_message->recipients_num = recipients_num;
+	new_message->recipients = recipients;
+	new_message->message = message;
+	new_message->next = NULL;
 
-  if (mailbox->number_of_messages < MAX_MESSAGE_COUNT){
-    if (!(mailbox->messages->message)){
-      mailbox->messages = new_message;
-      mailbox->last_msg = new_message;
-    }
-    else {
-      mailbox->last_msg->next = new_message;
-      mailbox->last_msg = new_message;
-    }
-    mailbox->number_of_messages += 1;
-  }
-  else {
-    printf("Error: mailbox is full\n");
-    return ERROR;
-  }
+	if (mailbox->number_of_messages < MAX_MESSAGE_COUNT)
+	{
+		if (!(mailbox->messages->message))
+		{
+			mailbox->messages = new_message;
+			mailbox->last_msg = new_message;
+		}
+		else
+		{
+			mailbox->last_msg->next = new_message;
+			mailbox->last_msg = new_message;
+		}
+		mailbox->number_of_messages += 1;
+	}
+	else
+	{
+		printf("Error: mailbox is full\n");
+		return ERROR;
+	}
 
-  return OK;
+	return OK;
 }
