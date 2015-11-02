0cbea54c0f31d03d0dd2be86916f8e0e2908e1cb
Sergio Gil <sergio.gil.luque@gmail.com>
2015-11-02 02:25:23 +0000

    Adding a test for sending a message

---
+++ test01.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <lib.h>
+#include <string.h>
+#include <mailboxlib.h>
+
+
+int main(int argc, char* argv[])
+{
+	if (argc <= 2)
+	{
+		printf ("Usage: %s MESSAGE RECIEVER_PID [ADDITIONAL_RECIEVER_PID]\n", argv[0]);
+		return 0;
+	}
+
+	char *message = argv[1];
+	int recieversPid[argc - 2];
+	int i;
+
+	for (i = 0; i < argc - 2; i++)
+	{
+		recieversPid[i] = atoi(argv[i + 2]);
+	}
+
+	printf("Test 01: Calling send_message\n");
+
+	printf("*Debug: send_message( message %s; pids %i, %i, %i, %i)\n", message, strlen(message), recieversPid[0], recieversPid[1],recieversPid[2]);
+	//char *test = "Hello, this is a test!";
+
+	/*int testPid[2];
+	testPid[0] = 1000;
+	testPid[1] = 1234;*/
+
+	send_message(message, strlen(message), recieversPid, argc - 2 );
+
+	return 0;
+}
