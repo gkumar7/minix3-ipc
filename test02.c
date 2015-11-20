712c8e43ae23cdc3ea3881b2bf67ab829dba5235
Geet Kumar <gkumar7@hawk.iit.edu>
2015-11-19 18:35:09 -0600

    test files from project 2

---
+++ test02.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <lib.h>
+#include <string.h>
+#include <mailboxlib.h>
+
+
+int main(int argc, char* argv[])
+{
+
+
+	if (argc <= 1)
+	{
+		printf ("Usage: %s PROCESS_PID\n", argv[0]);
+		return 0;
+	}
+
+	int processPid = atoi(argv[1]);
+	char destBuffer[MAX_MESSAGE_LEN];
+
+	printf("Test 02: Calling get_message\n");
+
+	if (receive_message(destBuffer, sizeof(destBuffer), processPid) == OK)
+	{
+		printf("***The process has received a new message***\n");
+		printf("The message is: %s\n", destBuffer);
+	}
+	else
+	{
+		printf("There are no messages for this process\n");
+	}
+
+	return 0;
+}
