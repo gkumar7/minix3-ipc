a8fa74e45d668aca514edfee780ad622595149a0
Sergio Gil <sergio.gil.luque@gmail.com>
2015-11-26 19:38:50 +0000

    Updating tests

--- tests/send_message.c
+++ tests/send_message.c
@@ -16,12 +16,12 @@ int main(int argc, char* argv[])
 
 	char* mailbox_name = "mailboxTest";
 	char* message_subject = "messageTestSubject";
-    char *message_data = "This is the content of the message";
+    char* message_data = "This is the content of the message";
     size_t mailboxNameLen = strlen(mailbox_name);
     size_t subjectLen = strlen(message_subject);
     size_t messageLen = strlen(message_data);
 
-	if (send_message(mailbox_name, message_subject, message_data, mailboxNameLen, subjectLen, messageLen);
+	if (send_message (mailbox_name, message_subject, message_data, mailboxNameLen, subjectLen, messageLen));
 	{
 		printf("*Error when creating mailbox");
 	}
